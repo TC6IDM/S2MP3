dfe7da15c4e2e25fee07e9bc627843d4fa55c923
TC6IDM <TCAND6@GMAIL.COM>
2023-03-14 01:59:40 -0400

    test

--- test.py
+++ test.py
@@ -22,12 +22,12 @@ def prYellow(skk): print("\033[93m{}\033[00m" .format(skk))
 def prCyan(skk): print("\033[96m{}\033[00m" .format(skk))
 def prLightPurple(skk): print("\033[94m{}\033[00m" .format(skk)) 
 def prPurple(skk): print("\033[95m{}\033[00m" .format(skk))
-html = urllib.request.urlopen("https://www.youtube.com/results?search_query=shorts").read().decode()
+# html = urllib.request.urlopen("https://www.youtube.com/results?search_query=shorts").read().decode()
 # with open(f"C:\\Users\\Owner\\Desktop\\Youtube-DL-Code\\LW6QSPI0NN8F197NF9LAQYQCF html.txt") as f:
 #     html = f.readline()
-video_ids = re.findall(r'"videoRenderer":{"videoId":"(\S{11})',html)
-video_titles = re.findall(r'}]},"title":{"runs":\[{"text":"(.*?)"}]',html)
-video_lengths = re.findall(r'{"thumbnailOverlayTimeStatusRenderer":{"text":{"accessibility":{"accessibilityData":{"label":"(.*?)"}},',html) 
+# video_ids = re.findall(r'"videoRenderer":{"videoId":"(\S{11})',html)
+# video_titles = re.findall(r'}]},"title":{"runs":\[{"text":"(.*?)"}]',html)
+# video_lengths = re.findall(r'{"thumbnailOverlayTimeStatusRenderer":{"text":{"accessibility":{"accessibilityData":{"label":"(.*?)"}},',html) 
 
 # with open(f"C:\\Users\\Owner\\Desktop\\Youtube-DL-Code\\LW6QSPI0NN8F197NF9LAQYQCF ids_titles_lengths.txt") as f:
 #     lines = f.readlines()
@@ -37,6 +37,21 @@ video_lengths = re.findall(r'{"thumbnailOverlayTimeStatusRenderer":{"text":{"acc
         
 # for i in range(len(lines[1])):
 #     print("https://www.youtube.com/watch?v="+lines[0][i],lines[1][i],lines[2][i])
-print(len(video_ids),len(video_titles),len(video_lengths))
-for i in range(len(video_ids)):
-    print("https://www.youtube.com/watch?v="+video_ids[i],video_titles[i],video_lengths[i])
+
+
+# print(len(video_ids),len(video_titles),len(video_lengths))
+# for i in range(len(video_ids)):
+#     print("https://www.youtube.com/watch?v="+video_ids[i],video_titles[i],video_lengths[i])
+
+def gatekeep(blacklist,artist,song,yttitle):
+    for i in blacklist:
+        if (not (i not in yttitle.lower() or (i in artist.lower() or i in song.lower()))): return False #if word is in the title, it must be in the artist or song name
+    return True
+
+blacklist = ["clean","8d"]
+
+print(gatekeep(blacklist,"Clean Bandit","Solo (feat. Demi Lovato)","Clean Bandit - Solo (feat. Demi Lovato) (Official Video)")) #true
+print(gatekeep(blacklist,"Bandit","Solo (feat. Demi Lovato)","Clean Bandit - Solo (feat. Demi Lovato) (Official Video)")) #false
+print(gatekeep(blacklist,"Bandit","Solo (feat. Demi Lovato)","Bandit - Solo (feat. Demi Lovato) (Official Video)"))#true
+print(gatekeep(blacklist,"Clean Bandit","Solo (feat. Demi Lovato)","Bandit - Solo (feat. Demi Lovato) (Official Video)"))#true
+
