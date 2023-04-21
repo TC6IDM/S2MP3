d75c636dff5af698a31f876e13a0fac42ca65d8c
TC6IDM <TCAND6@GMAIL.COM>
2023-04-21 00:10:11 -0400

    last edits for now :(

--- README.md
+++ README.md
@@ -49,10 +49,43 @@ Traceback (most recent call last):
     raise HTTPError(req.full_url, code, msg, hdrs, fp)
 urllib.error.HTTPError: HTTP Error 502: Bad Gateway
 
+RROR: [youtube] lE-GhpoL3c4: Video unavailable. This video is not available
+
+
+Editing Metadata
+Traceback (most recent call last):
+  File "C:\Users\Owner\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\mutagen\_util.py", line 251, in _openfile
+    fileobj = open(filename, "rb+" if writable else "rb")
+FileNotFoundError: [Errno 2] No such file or directory: ''
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "c:\Users\Owner\Desktop\S2MP3\SpotifyToMp3.py", line 148, in <module>
+    while not downloadPlaylist(stressTestPlaylist): pass
+  File "c:\Users\Owner\Desktop\S2MP3\SpotifyToMp3.py", line 129, in downloadPlaylist
+    song.getBestVideo().download()
+  File "c:\Users\Owner\Desktop\S2MP3\YoutubeSong.py", line 127, in download
+    self.parent.setFileData(newDestination)
+  File "c:\Users\Owner\Desktop\S2MP3\Song.py", line 173, in setFileData
+    audio = MP3(file, ID3=EasyID3)
+  File "C:\Users\Owner\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\mutagen\_file.py", line 48, in __init__
+    self.load(*args, **kwargs)
+  File "C:\Users\Owner\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\mutagen\_util.py", line 154, in wrapper
+    with _openfile(self, filething, filename, fileobj,
+  File "C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.3056.0_x64__qbz5n2kfra8p0\lib\contextlib.py", line 135, in __enter__
+    return next(self.gen)
+  File "C:\Users\Owner\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\mutagen\_util.py", line 272, in _openfile
+    raise MutagenError(e)
+mutagen.MutagenError: [Errno 2] No such file or directory: ''
+
 BOTTLENECKS:
 1. getting results
 2. converting to mp3 
 3. the pause before downloading starts
+
+#cd D:\Songs\test\Road-Kill
+#python -m spotdl https://open.spotify.com/playlist/24Qq6TevVwM8N8F0yjRily?si=5ff918e3c1e94d8b
 
 PACKAGE EDITS:
 1. 
@@ -96,4 +129,9 @@ line 380:
 3. 
 C:\Users\Owner\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\mutagen\easyid3.py
 line 276:
-    return ["NULL"] if "TDRC" in id3 else [stamp.text for stamp in id3["TDRC"].text]
+    return ["NULL"] if "TDRC" not in id3 else [stamp.text for stamp in id3["TDRC"].text]
+
+4. 
+C:\Users\Owner\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\site-packages\mutagen\_util.py
+line 537:
+    return ["NULL"] if key not in self.__dict else self.__dict[key]
--- SpotifyToMp3.py
+++ SpotifyToMp3.py
@@ -1,5 +1,6 @@
 import re
 from os.path import exists
+import subprocess
 import time
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -8,10 +9,12 @@ from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
 from extraUtil import *
 
-def get_playlist_tracks(sp,username,playlist_id): #slowest part of the program
-    playlistLength = sp.user_playlist(username,playlist_id)['tracks']['total']
+def get_playlist_tracks(sp,username,playlist_id,album): #slowest part of the program
+    if not album: playlistLength = sp.user_playlist(username,playlist_id)['tracks']['total']
+    else: playlistLength = sp.album(playlist_id)['tracks']['total']
     print(f'Fetching songs {0} / {playlistLength} {0.00}%                        ',end='\r')
-    results = sp.user_playlist_tracks(username,playlist_id)
+    if not album: results = sp.user_playlist_tracks(username,playlist_id)
+    else: results = sp.album_tracks(playlist_id)
     tracks = results['items']
     while results['next']:
         results = sp.next(results)
@@ -23,44 +26,51 @@ def get_playlist_tracks(sp,username,playlist_id): #slowest part of the program
     return tracks
 
 def checkPlaylist(playlist,session):
+    album=False
     if match := re.match(r"https://open.spotify.com/playlist/(.*)\?", playlist):
         playlist_uri = match.groups()[0]
     else:
-        raise ValueError("Expected format: https://open.spotify.com/playlist/...")
+        if match := re.match(r"https://open.spotify.com/album/(.*)\?", playlist):
+            album = True
+            playlist_uri = match.groups()[0]
+        else:
+            raise ValueError("Expected format: https://open.spotify.com/(playlist/album)/...")
     # get list of tracks in a given playlist (note: max playlist length 100)
     
-    playlistName =deleteBadCharacters(removeSymbols(session.user_playlist(USERNAME,playlist_uri)["name"]))
-    print("Fetching spotify playlist - "+playlistName+ " https://open.spotify.com/playlist/"+playlist_uri)
-    tracks = get_playlist_tracks(session,USERNAME,playlist_uri)
-    songList =[]
+    if not album : 
+        playlistName =deleteBadCharacters(removeSymbols(session.user_playlist(USERNAME,playlist_uri)["name"]))
+        print("Fetching spotify playlist - "+playlistName+ " https://open.spotify.com/playlist/"+playlist_uri)
+        
+    else : 
+        playlistName =deleteBadCharacters(removeSymbols(session.album(playlist_uri)["name"]))
+        print("Fetching spotify Album - "+playlistName+ " https://open.spotify.com/album/"+playlist_uri)
+        
+    tracks = get_playlist_tracks(session,USERNAME,playlist_uri,album)
     
+    songList =[]
+    # print(tracks)
     for index,track in enumerate(tracks,start=1):
-        thisTrack = Song(track["track"],index,playlistName)
+        if not album: thisTrack = Song(track["track"],index,playlistName)
+        else: thisTrack = Song(track,index,playlistName)
         songList.append(thisTrack)
     prGreen(f'Playlist {playlistName} has ben fetched with {len(songList)} songs')
     return playlistName,songList
 
-def removePartials(songList):
-    parentFolder = songList[0].parentFolder
-    if not os.path.exists(parentFolder):
-        os.makedirs(parentFolder)
-        
-    removeFiles = []
-    for i,file in enumerate(os.listdir(parentFolder),start=1):
-        print(f'validating folder {i} / {len(os.listdir(parentFolder))} ({round(100 * i / len(os.listdir(parentFolder)),2)}%)     ',end='\r')
-        audio_file = parentFolder+"\\"+file
-        if not re.match(r"^\(\d{4}\).+\.mp3$",file):
-            removeFiles.append(audio_file)
-            continue
-        songNumber = int(re.search(r'\((\d{4})\)', file).group(1))
-        if not songList[songNumber-1].destination+'.mp3' == audio_file:
-            if "$$" not in audio_file: 
-                removeFiles.append(audio_file) # https://github.com/yt-dlp/yt-dlp/issues/6847
-                continue
-        audio = MP3(audio_file, ID3=EasyID3)
-        if audio=={}:
-            removeFiles.append(audio_file)
-            continue
+def removeIndividually(file,parentFolder,removeFiles,songList):
+    audio_file = parentFolder+"\\"+file
+    if not re.match(r"^\(\d{4}\).+\.mp3$",file):
+        removeFiles.append(audio_file)
+        return removeFiles
+    songNumber = int(re.search(r'\((\d{4})\)', file).group(1))
+    if not songList[songNumber-1].destination+'.mp3' == audio_file:
+        if "$$" not in audio_file: 
+            removeFiles.append(audio_file) # https://github.com/yt-dlp/yt-dlp/issues/6847
+            return removeFiles
+    # print(audio_file)
+    audio = MP3(audio_file, ID3=EasyID3)
+    if audio=={}:
+        removeFiles.append(audio_file)
+        return removeFiles
         # print(audio['date'][0] == songList[songNumber-1].releaseDate or audio['date'][0] == re.search('^([^\\-]*)',songList[songNumber-1].releaseDate).group(1))
         # if not (audio['date'][0] == songList[songNumber-1].releaseDate or audio['date'][0] == re.search('^([^\\-]*)',songList[songNumber-1].releaseDate).group(1)):
         #     print(audio['date'])
@@ -68,16 +78,28 @@ def removePartials(songList):
         #     print(songList[songNumber-1].releaseDate)
         #     print(re.search('^([^\\-]*)',songList[songNumber-1].releaseDate).group(1))
         #     time.sleep(5)
-        if (not (audio['artist'][0] == songList[songNumber-1].trackArtists) or 
-            not (audio['title'][0] == songList[songNumber-1].trackName) or 
-            not (audio['albumartist'][0] == songList[songNumber-1].albumArtists) or 
-            not (audio['album'][0] == songList[songNumber-1].albumName) or 
-            not (audio['date'][0] == songList[songNumber-1].releaseDate or audio['date'][0] == re.search('^([^\\-]*)',songList[songNumber-1].releaseDate).group(1)) or 
-            not (audio['discnumber'][0] == str(songList[songNumber-1].discnumber)) or 
-            not (audio['tracknumber'][0] == str(songList[songNumber-1].tracknumber))):
-            removeFiles.append(audio_file)
-                    
-
+    # print(audio)
+    if ('artist'  in audio and not (audio['artist'][0] == songList[songNumber-1].trackArtists) or 
+        'title'  in audio and not (audio['title'][0] == songList[songNumber-1].trackName) or 
+        'albumartist'  in audio and not (audio['albumartist'][0] == songList[songNumber-1].albumArtists) or 
+        'album'  in audio and not (audio['album'][0] == songList[songNumber-1].albumName) or 
+        'date'  in audio and not (audio['date'][0] == songList[songNumber-1].releaseDate or audio['date'][0] == re.search('^([^\\-]*)',songList[songNumber-1].releaseDate).group(1)) or 
+        'discnumber'  in audio and not (audio['discnumber'][0] == str(songList[songNumber-1].discnumber)) or 
+        'tracknumber'  in audio and not (audio['tracknumber'][0] == str(songList[songNumber-1].tracknumber))):
+        removeFiles.append(audio_file)
+        return removeFiles
+    
+    return removeFiles
+def removePartials(songList):
+    parentFolder = songList[0].parentFolder
+    if not os.path.exists(parentFolder):
+        os.makedirs(parentFolder)
+        
+    removeFiles = []
+    for i,file in enumerate(os.listdir(parentFolder),start=1):
+        print(f'validating folder {i} / {len(os.listdir(parentFolder))} ({round(100 * i / len(os.listdir(parentFolder)),2)}%)     ',end='\r')                 
+        removeFiles = removeIndividually(file,parentFolder,removeFiles,songList)
+        
     print()
     for file in removeFiles:
         if os.path.exists(file):
@@ -94,7 +116,9 @@ def downloadPlaylist (currentPlaylist):
     )
     # create spotify session object
     session = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    validPlaylist=True if currentPlaylist.startswith("https://open.spotify.com/playlist/") or currentPlaylist.startswith("https://open.spotify.com/album/") else False
     validPlaylist=True if currentPlaylist.startswith("https://open.spotify.com/playlist/") else False
+
     if not validPlaylist: return playlistFinished
     playlistName,songList = checkPlaylist(currentPlaylist,session)
     removePartials(songList)
@@ -115,12 +139,18 @@ def downloadPlaylist (currentPlaylist):
 
 def run():
     file = open(PLAYLIST_FILE_NAME,'r')
+    line = ""
     for currentPlaylist in file.readlines():
-        while not downloadPlaylist(currentPlaylist): pass
+        subprocess.run(["python" ,"-m" ,"spotdl", re.sub(" .*", "", currentPlaylist).strip() ])
+        #regex remove everything after the first space
+        # line += re.sub(" .*", "", currentPlaylist).strip()+", "
+        # while not downloadPlaylist(currentPlaylist): pass
+        pass
     prPurple("DONE ALL PLAYLISTS")
+    # print(line)
     
 if __name__ == "__main__":
-    stressTest = True
+    stressTest = False
     stressTestPlaylist = "https://open.spotify.com/playlist/0gnLoConJALD8SVqZyP8I1?si=149eda709fcc4426"
     if stressTest:
         while not downloadPlaylist(stressTestPlaylist): pass
